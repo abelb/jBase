19df658778db6f344508c7ac8f350048608144bf
jwolf <wolf.jay@gmail.com>
2011-03-14 21:31:22 -0700

    adding example layout meant for a foundation to create custom panels layouts

---
+++ panels/layouts/main-layout/main_layout.tpl.php
@@ -0,0 +1,38 @@
+<div id="<?php if (!empty($css_id)) { print $css_id; } ?>" class="main-layout">
+  
+  <?php if ($content['top']): ?>
+    <div class="top-container">
+      <?php print $content['top']; ?>
+    </div><!-- /panel-top -->
+  <?php endif; ?>
+  
+  <?php $column_content = $content['left_first'] || $content['right_first'] || $content['left_seond'] || $content['right_second']; ?>
+  <?php if ($column_content): ?>
+      
+    <div class="container-row clearfix">
+      <div class="left">
+        <?php print $content['left_first']; ?>
+      </div><!-- /left -->
+      <div class="right">
+        <?php print $content['right_first']; ?>
+      </div><!-- /right -->
+    </div><!-- /container-row first -->
+      
+    <div class="container-row clearfix">
+      <div class="left">
+        <?php print $content['left_second']; ?>
+      </div><!-- /left -->
+      <div class="right">
+        <?php print $content['right_second']; ?>
+      </div><!-- /right -->
+    </div><!-- /container-row last -->
+      
+  <?php endif; ?>
+  
+  <?php if ($content['bottom']): ?>
+    <div class="bottom-content">
+      <?php print $content['bottom']; ?>
+    </div>
+  <?php endif; ?>
+
+</div><!-- /main-layout -->
